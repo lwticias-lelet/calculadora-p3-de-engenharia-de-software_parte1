4cd9d0ec71f536b20789999a4a56ea0af806af59
lwticias-lelet <leticiadaraujo1@gmail.com>
2023-12-02 16:47:36 -0300

    update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,7 +10,7 @@ function clean(){
 }
 function back(){
     var resultado=document.getElementById('resultado').innerHTML;
-    document.getElementById("resultado").innerHTML =resultado.substring(0, resultado ,length -1);
+    document.getElementById("resultado").innerHTML =resultado.substring(0, resultado. length -1);
 
 }
 function calcular(){
