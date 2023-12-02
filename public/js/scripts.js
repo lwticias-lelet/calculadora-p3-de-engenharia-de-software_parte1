d3ea29c583a37325f1cf1299865705e51c77b588
lwticias-lelet <leticiadaraujo1@gmail.com>
2023-12-02 16:55:35 -0300

    update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,10 +13,20 @@ function back(){
     document.getElementById("resultado").innerHTML =resultado.substring(0, resultado. length -1);
 
 }
-function calcular(){
+function calcular() {
     var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML =eval(resultado);
-    
+
+    try {
+        var resultadoCalculado = eval(resultado);
+
+        if (isNaN(resultadoCalculado) || resultadoCalculado === undefined) {
+            throw new Error("Expressão inválida");
+        }
+
+        document.getElementById('resultado').innerHTML = resultadoCalculado;
+    } catch (error) {
+        document.getElementById('resultado').innerHTML = "Nada para calcular";
+    }
 }
 
 module.exports = {insert, clean, back, calcular}
